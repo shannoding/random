63de5d84a45a6a91eb78c3049d8ccb6dec16d6a8
shannoding <shannon_ding@yahoo.com>
2018-08-15 01:21:58 -0700

    database reading test

--- databased.js
+++ databased.js
@@ -1,9 +1,18 @@
 var input = document.getElementById("input");
 var submit = document.getElementById("submit");
 var results = document.getElementById("results");
+
+var database = firebase.database();
+
 submit.addEventListener("click", update);
 
 function update() {
 	var request = input.value.trim();
-	results.textContent = request;
+	var v = database.ref(request).once("value");
+	if (v.exists()) {
+		results.textContent = v.val();
+	}
+	else {
+		results.textContent = "doesn't exist";
+	}
 }
